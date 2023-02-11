41fe7d255b5e3b92449e0a9ae46deca4b6c81140
wasiuralukasz <lkasz6233@gmail.com>
2023-02-11 22:10:33 +0100

    Added move ball and border detection

--- Bouncing-ball/MainWindow.xaml.cs
+++ Bouncing-ball/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Bouncing_ball
 {
@@ -20,9 +21,39 @@ namespace Bouncing_ball
     /// </summary>
     public partial class MainWindow : Window
     {
+        double moveX = 0.1, moveY = 0.1;
+        DispatcherTimer timer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+            timer.Tick += timer_Tick;
+            timer.Interval = TimeSpan.FromMicroseconds(500);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Canvas.SetLeft(Ball, Canvas.GetLeft(Ball) + moveX);
+            Canvas.SetTop(Ball, Canvas.GetTop(Ball) + moveY);
+
+            if(Canvas.GetLeft(Ball) > MyCanvas.ActualWidth - Ball.Width || Canvas.GetLeft(Ball) <= 0)
+            {
+                moveX *= -1;
+            }
+            if (Canvas.GetTop(Ball) > MyCanvas.ActualHeight - Ball.Height || Canvas.GetTop(Ball) <= 0)
+            {
+                moveY *= -1;
+            }
+        }
+
+        public void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
         }
     }
 }
